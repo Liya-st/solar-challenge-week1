2e829300a608f44a3d363a30a631c0ab76a81cae
Liya H <liyatsegaye2301@gmail.com>
2025-05-18 15:59:55 +0300

    update:clean script

--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -9,6 +9,7 @@ def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     negative_cols = ['GHI', 'DNI', 'DHI']
     for col in negative_cols:
         df = df[df[col] >= 0]
+        df[col] = df[col].where(df[col] >= 0, 0)
 
     df = df.reset_index(drop=True)
 
