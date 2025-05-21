cfeb8830ec60ecdaea38d4faee2c2f58fbed8677
Liya H <liyatsegaye2301@gmail.com>
2025-05-21 15:07:59 +0300

    update: eda-togo

--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -1,15 +1,14 @@
 import pandas as pd
 
-def clean_data(df: pd.DataFrame) -> pd.DataFrame:
 
 
-    if 'Comments' in df.columns:
-        df = df.drop(columns=['Comments'])
-
+def clean_data(df):
     negative_cols = ['GHI', 'DNI', 'DHI']
     for col in negative_cols:
-        df = df[df[col] >= 0]
-        df[col] = df[col].where(df[col] >= 0, df[col].median)
-    df = df.reset_index(drop=True)
+        if col in df.columns:
+            median_value = df[df[col] >= 0][col].median()
+            df[col] = df[col].apply(lambda x: median_value if x < 0 else x)
+        else:
+            print(f"Column '{col}' not found in dataframe.")
+    return df.reset_index(drop=True)
 
-    return df
